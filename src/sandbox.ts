19fea6b835791a90bac8b8f0cec9cfb24f239a72
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-20 15:11:11 +0100

    learn typescript function signatures

---
+++ src/sandbox.ts
@@ -0,0 +1,27 @@
+// let greet: Function;
+
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+};
+
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne, numTwo, action) => {
+  if (action == "add") {
+    return numOne + numTwo;
+  } else {
+    return numOne + numTwo;
+  }
+};
+
+let logDetails: (obj: { name: string; age: number }) => void;
+
+type person = { name: string; age: number };
+
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja.age} old`);
+};
+
+
