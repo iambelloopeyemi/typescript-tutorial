aa5b9a0ef697efb29ac569a5905722eb0ab650b1
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-20 15:06:36 +0100

    learn typescript function-signatures

---
+++ public/sandbox.js
@@ -0,0 +1,16 @@
+"use strict";
+// let greet: Function;
+let greet;
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action == "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne + numTwo;
+    }
+};
+let logDetails;
