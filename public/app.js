2b474e1dce9332ea987b3c4910470d968c4a50e0
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-26 11:34:46 +0100

    learn classes in typescript

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
 "use strict";
+// Classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+// console.log(invOne, invTwo)
+// DOM and Type Casting
 // const anchor = document.querySelector("a")!;
 // // if (anchor) {
 // //   console.log(anchor.href);
