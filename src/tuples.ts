750bc16619d2db0bd1b66b89585ed4de281a251d
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-26 17:17:12 +0100

    learn typescript tuples

---
+++ src/tuples.ts
@@ -0,0 +1,11 @@
+let arr = ["ryu", 25, true];
+arr[0] = false;
+arr[1] = "yoshi";
+arr = [10, false, "yoshi"];
+
+let tup: [string, number, boolean] = ["ryu", 25, true];
+tup[0] = "ken";
+tup[1] = 30;
+
+let student: [string, number];
+student = ["mario", 25];
